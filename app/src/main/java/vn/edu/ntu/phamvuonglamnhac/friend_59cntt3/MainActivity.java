0fbf3ddf80a28eb25c559a6ede57bd06b42feceb
Administrator <nhacpham550212@gmail.com>
2020-05-16 11:03:29 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/ntu/phamvuonglamnhac/friend_59cntt3/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.phamvuonglamnhac.friend_59cntt3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
